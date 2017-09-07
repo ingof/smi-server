2a4de4b2ffddd1d21eaf1fc953a46afe50007108
ingof <ingo.fischer@arcormail.de>
2017-09-07 19:34:10 +0200

    .

--- source/typesdef.h
+++ source/typesdef.h
@@ -1,10 +1,10 @@
-/* types.h */
-#ifndef TYPES_H
-#define TYPES_H
+/* typesdef.h */
+#ifndef TYPESDEF_H
+#define TYPESDEF_H
 
 
 typedef unsigned char uint8_t;
 typedef unsigned int  unint16_t;
 
 
-#endif /* types.h */
+#endif /* typesdef.h */
