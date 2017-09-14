9422598dd62da7ccfd47d4b9238cf80cc72b2679
ingof <ingo.fischer@arcormail.de>
2017-09-14 21:06:27 +0200

    config tests

---
+++ source/parseconf.c
@@ -0,0 +1,10 @@
+/* smi-server */
+//#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>			/* syslog */
+
+int main(int argc, char *argv[]) {
+
+	exit(EXIT_SUCCESS);
+}
---
+++ source/parseconf.h
@@ -0,0 +1,9 @@
+/* parseconf.h */
+#ifndef PARSECONF_H
+#define PARSECONF_H
+
+
+/* functions */
+
+
+#endif /* parseconf.h */
