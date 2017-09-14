da4bba162cf71e630595eb376f07046cdb9bb843
ingof <ingo.fischer@arcormail.de>
2017-09-14 21:16:48 +0200

    .

--- source/parseconf.c
+++ source/parseconf.c
@@ -1,10 +1,52 @@
-/* smi-server */
-//#include <sys/types.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>			/* syslog */
 
-int main(int argc, char *argv[]) {
 
-	exit(EXIT_SUCCESS);
+int main()
+{
+   puts("jashh");
+   char str1[10], str2[10], str3[10];
+   char name[20], value[20];
+   char line[80];
+   int year;
+   FILE * fp;
+
+   fp = fopen ("smi-server.conf", "w+");
+   fputs("global=1", fp);
+   fputs("# comment", fp);
+   fputs("", fp);
+   fputs("[Person]", fp);
+   fputs(" name=Max", fp);
+   fputs(" surname=Mustermann", fp);
+   fputs("[car]", fp);
+   fputs(" name=Ferrari", fp);
+   fputs(" model=F40", fp);
+
+   rewind(fp);
+
+//   fscanf(fp, "%s = %s", str1, str2);
+   printf("Read String1 |%s|\n", str1 );
+   printf("Read String2 |%s|\n\n", str2 );
+
+//   fscanf(fp, "%s = %s", str1, str2);
+   printf("Read String1 |%s|\n", str1 );
+   printf("Read String2 |%s|\n\n", str2 );
+
+//   fscanf(fp, "%s = %s", str1, str2);
+   printf("Read String1 |%s|\n", str1 );
+   printf("Read String2 |%s|\n\n", str2 );
+
+//   rewind(fp);
+
+    sscanf(line, "%s %s", name, value);
+    printf("Read: |%s|%s|\n\n", name, value );
+
+	sscanf(line, "%s %s", name, value);
+	printf("Read: |%s|%s|\n\n", name, value );
+
+	sscanf(line, "%s %s", name, value);
+	printf("Read: |%s|%s|\n\n", name, value );
+	fclose(fp);
+
+   return(0);
 }
