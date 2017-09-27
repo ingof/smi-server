75f845b1b9562d659642e63b2bc36ca93b3badb9
ingof <ingo.fischer@arcormail.de>
2017-09-27 19:40:54 +0200

    debug parse config

--- source/parseconf.c
+++ source/parseconf.c
@@ -38,37 +38,28 @@ int parseConfFile(void) {
 
 int parseConfLine(char* line) {
   char* remark;// initialisieren und ersten Abschnitt erstellen
-  // int oldLine;// initialisieren und ersten Abschnitt erstellen
 
-  // printf(" Text     : %s (%d)\n", line, line[0]);
-  // strcpy(oldLine, line);
-
-  if (strchr(line,(int)'#')) {
   // remark-line cut line at "#" char
-    remark=strchr(line, 35); //35="#"
-    if ( remark != NULL ) {
-      return 0;
-    }
-  }
+    if (strchr(line,(int)'#')) {
+        remark=strchr(line, 35); //35="#"
+        if ( remark != NULL ) {
+            return 0;
+        }
+    } else {
 
-  if ((strchr(line, (int) '[')!=NULL)&&(strchr(line,(int)']')!=NULL)) {
-    char tmpLine[40]="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-    strncpy(tmpLine, (strchr(line, (int)'[')+1), ((strchr(line,(int)']')-strchr(line, (int) '[')-1)) );
-    syslog(LOG_DEBUG, "\tSection : %s\n", tmpLine);
-    }
+      // get sections
+      if ((strchr(line, (int) '[')!=NULL)&&(strchr(line,(int)']')!=NULL)) {
+        char tmpLine[40]="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+        strncpy(tmpLine, (strchr(line, (int)'[')+1), ((strchr(line,(int)']')-strchr(line, (int) '[')-1)) );
+        syslog(LOG_DEBUG, "\tSection : %s\n", tmpLine);
+      }
 
-  // get values
-  if (strchr(line,(int)'=')) {
-    // syslog(LOG_DEBUG, "Value  :");
-    ptr = strtok(line, "=");
-    ptr2 = strtok(NULL, "=");
-    // if (line[0]!=35) {
-    //   while(ptr != NULL) {
-      // naechsten Abschnitt erstellen
-      syslog(LOG_DEBUG, "\t  Value : \"%s\" : \"%s \"", ptr, ptr2);
-    //   ptr2 = strtok(NULL, "=");
-    //   }
-    // }
+      // get values
+      if (strchr(line,(int)'=')) {
+        ptr = strtok(line, "=");
+        ptr2 = strtok(NULL, "=");
+        syslog(LOG_DEBUG, "\t  Value : \"%s\" : \"%s \"", ptr, ptr2);
+      }
   }
 
   // Get values
