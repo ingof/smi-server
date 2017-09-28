fbdd547b0f7aff54c2c36a877385aa87f97eb000
ingof <ingo.fischer@arcormail.de>
2017-09-28 17:52:27 +0200

    debug parse config

--- source/parseconf.c
+++ source/parseconf.c
@@ -45,8 +45,7 @@ int parseConfLine(char* line) {
     remark=strchr(line,(int)'#');
     if ( remark != NULL ) {
         // skip comments
-        syslog(LOG_DEBUG, "DEBUG: skip comment (%d) |%s|", remark, line);
-        line[remark]="\0";
+        // line[(remark-1)]="\0";
         syslog(LOG_DEBUG, "DEBUG: skip comment (%d) |%s|", remark, line);
         // exit(EXIT_FAILURE);
     }
