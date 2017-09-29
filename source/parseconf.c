6113521455d45680f713019c32ab9e5fe145419d
ingof <ingo.fischer@arcormail.de>
2017-09-29 19:08:57 +0200

    parse config

--- source/parseconf.c
+++ source/parseconf.c
@@ -53,7 +53,7 @@ int parseConfLine(char* line) {
     strncat(tmpLine, line, 39);
     syslog(LOG_DEBUG,"DEBUG:    Line: |%s", tmpLine);
     linePtr = strtok(tmpLine, "#");
-    strncpy(line,*linePtr,40);
+    strncpy(line,tmpLine,40);
     syslog(LOG_DEBUG,"DEBUG: LineOut: |%s", line);
 
     // get sections
