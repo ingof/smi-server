6c166dbe78c78c5335108ea4d89650ceb2c8d255
ingof <ingo.fischer@arcormail.de>
2017-10-05 20:02:56 +0200

    webserver

--- source/smi-server.c
+++ source/smi-server.c
@@ -145,63 +145,62 @@ int main(int argc, char *argv[]) {
 		}
 		/* webserver */
 		{
-			{
-				tmpListen=listen(mySocket, 10);
-				if (tmpListen < 0) {
-					syslog(LOG_NOTICE, "NOTICE: webserver listen: %d", tmpListen);
+			tmpListen=listen(mySocket, 10);
+			if (tmpListen < 0) {
+				syslog(LOG_NOTICE, "NOTICE: webserver listen: %d", tmpListen);
+				exit(1);
+			}
+
+			setNonblocking(mySocket);
+			clientAddrLen = sizeof( (struct sockaddr *) &clientAddress);
+
+			if ((newSocket = accept(mySocket, (struct sockaddr *) &clientAddress, &clientAddrLen)) < 0) {
+				if (errno == EAGAIN) { // no data available
+				} else {
+					syslog(LOG_NOTICE, "NOTICE: webserver accept %d", newSocket);
 					exit(1);
 				}
+			} else { // data available
+				if (newSocket <= 0){
+					syslog(LOG_DEBUG, "webserver connect:");
+				}
+				/* receive header */
+				// memset(bufferHTTP, 0, bufsize);
+				// fill_n(bufferHTTP, 0, bufsize);
+				for (loop2=0;loop2<bufSize;loop2++) {
+					bufferHTTP[loop2]=0;
+				}
 
-				setNonblocking(mySocket);
-				clientAddrLen = sizeof( (struct sockaddr *) &clientAddress);
+				recv(newSocket, bufferHTTP, bufSize, 0);
+				// printf("%s*ENDE*", bufferHTTP);
+				// getPostData(bufferHTTP,bufsize);
+				logBufferAscii(bufferHTTP,bufSize);
+				getPostData(bufferHTTP,bufSize);
+				logBufferAscii(bufferHTTP,bufSize);
 
-				if ((newSocket = accept(mySocket, (struct sockaddr *) &clientAddress, &clientAddrLen)) < 0) {
-						if (errno == EAGAIN) { // no data available
-						} else {
-							syslog(LOG_NOTICE, "NOTICE: webserver accept %d", newSocket);
-							exit(1);
-						}
-					} else { // data available
-					if (newSocket <= 0){
-						syslog(LOG_DEBUG, "webserver connect:");
-					}
-					/* receive header */
-					// memset(bufferHTTP, 0, bufsize);
-					// fill_n(bufferHTTP, 0, bufsize);
-					for (loop2=0;loop2<bufSize;loop2++) {
-						bufferHTTP[loop2]=0;
-					}
+				/* send response */
+				write(newSocket, "HTTP/1.1 200 OK\r\n", 17);
+				write(newSocket, "Content-length: 111\r\n", 21);
+				write(newSocket, "Content-Type: text/html\r\n\r\n", 27);
+				write(newSocket, "<html>\r\n",8);
+				write(newSocket, " <body>\r\n",9);
+				write(newSocket, "  <h1>Found</h1>\r\n",18);
+				write(newSocket, "  <p>The requested URL was found on this server ;-)</p>\r\n",57);
+				write(newSocket, " </body>\r\n",10);
+				write(newSocket, "</html>\r\n",9);
 
-					recv(newSocket, bufferHTTP, bufSize, 0);
-					// printf("%s*ENDE*", bufferHTTP);
-					// getPostData(bufferHTTP,bufsize);
-					logBufferAscii(bufferHTTP,bufSize);
-					getPostData(bufferHTTP,bufSize);
-					logBufferAscii(bufferHTTP,bufSize);
-
-					/* send response */
-					write(newSocket, "HTTP/1.1 200 OK\r\n", 17);
-					write(newSocket, "Content-length: 111\r\n", 21);
-					write(newSocket, "Content-Type: text/html\r\n\r\n", 27);
-					write(newSocket, "<html>\r\n",8);
-					write(newSocket, " <body>\r\n",9);
-					write(newSocket, "  <h1>Found</h1>\r\n",18);
-					write(newSocket, "  <p>The requested URL was found on this server ;-)</p>\r\n",57);
-					write(newSocket, " </body>\r\n",10);
-					write(newSocket, "</html>\r\n",9);
-
-					/* TODO: in PHP post-data will be send only after
-					receiving the 200-OK-Header. Add or use the second buffer only!
-					*/
-					// /* receive posted data */
-					// memset(bufferHTTP, 0, bufsize);
-					// recv(new_socket, bufferHTTP, bufsize, 0);
-					// printf("%s*ENDE*\n", bufferHTTP);
-					// printBuffer(bufferHTTP,bufsize);
-					// fflush(stdout);
-					/* close this socket */
-					close(newSocket);
-
+				/* TODO: in PHP post-data will be send only after
+				receiving the 200-OK-Header. Add or use the second buffer only!
+				*/
+				// /* receive posted data */
+				// memset(bufferHTTP, 0, bufsize);
+				// recv(new_socket, bufferHTTP, bufsize, 0);
+				// printf("%s*ENDE*\n", bufferHTTP);
+				// printBuffer(bufferHTTP,bufsize);
+				// fflush(stdout);
+				/* close this socket */
+				close(newSocket);
+			}
 		} /* webserver */
 
 	}
